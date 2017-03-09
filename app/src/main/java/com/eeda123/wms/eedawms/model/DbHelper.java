3b5e9cb160664bc4fc9cf69a41e7cc0adf03fce5
kevin <kevin.dou@eeda123.com>
2017-03-09 11:44:15 +0800

    创建数据库优化

--- app/src/main/java/com/eeda123/wms/eedawms/model/DbHelper.java
+++ app/src/main/java/com/eeda123/wms/eedawms/model/DbHelper.java
@@ -20,7 +20,7 @@ public class DbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         //String sql = "create table gate_in(id INTEGER PRIMARY KEY AUTOINCREMENT, qr_code varchar(200), part_no varchar(60), quantity int);";
-        /*String gete_in_sql = "CREATE TABLE `gate_in` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT , `qr_code`  varchar(255) NULL ," +
+        String gete_in_sql = "CREATE TABLE `gate_in` ( `id` INTEGER PRIMARY KEY AUTOINCREMENT , `qr_code`  varchar(255) NULL ," +
              " `part_no`  varchar(255) NULL , `quantity`  int(20) NULL , `shelves`  varchar(255) NULL ," +
              " `creator`  varchar(255) NULL  ,`create_time`  datetime NULL, return_flag varchar(2) default 'N'," +
              " move_flag varchar(2) default 'N') ";
@@ -36,7 +36,6 @@ public class DbHelper extends SQLiteOpenHelper {
            "  	`quantity` INT (20) NULL, 	`check_quantity` INT (20) NULL, `shelves` VARCHAR (255) NULL, " +
            " 	`creator` VARCHAR (255) NULL, 	`create_time` datetime NULL,`check_time` datetime NULL )";
         db.execSQL(inv_check_sql);
-        db.close();*/
     }
 
     @Override
